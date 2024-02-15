14bdc60b9d57e12f0e4ff8abb5f7e5ef63bb501f
Naidaly ruiz <naidalyruiz@gmail.com>
2024-02-15 16:26:07 +0100

    detail design check

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,46 +1,52 @@
-import '../scss/App.scss';
-import cover from '../images/cover.jpeg';
-import girlUser from '../images/girluser.png';
+import "../scss/App.scss";
+import cover from "../images/cover.jpeg";
+import girlUser from "../images/girluser.png";
 
 function Detail({ card }) {
-  
   return (
     <>
       <section className="detail">
-        <img className="detail___image" src={cover} alt="repo" /> 
-
-        <section className="info__container">
-          <section className="info-project">
-            <p className="subtitle">Personal Project Card</p>
-            <hr className="line" />
-
-            <h2 className="title">Elegant Workspace</h2>
-            <p className="slogan">Diseños Exclusivos</p>
-            <p className="desc">
-              Lorem, ipsum dolor sit amet consectetur 
+        <h4 className="subtitle">
+          Escaparate en linea para recoger ideas a través de la tecnología
+        </h4>
+        <img className="image" src={cover} alt="repo" />
+        <div className="projects">
+          <div className="firstBlockText">
+            <img
+              className="firstBlockText__img girlIMg"
+              src={girlUser}
+              alt="ProjectPhoto"
+            />
+            <h5 className="firstBlockText__job job">Full stack developer</h5>
+            <h3 className="firstBlockText__userName userName">
+              Emmelie Böjklund
+            </h3>
+          </div>
+          <div className="secondBlockText textCont">
+          <p className="subtitle lineText">Personal Project Card</p>
+          <hr className="line" />
+            <h4 className="secondBlockText__project projectText">
+              Elegeant Workspace
+            </h4>
+            <h5 className="secondBlockText__design slogan">
+              Diseños Exclusivos
+            </h5>
+            <p className="secondBlockText__desc desc">
+              Product Description Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Amet faucibus commodo tellus lectus lobortis.
+              Ultricies lacus, facilisis arcu ac mauris, laoreet sit.
             </p>
-            <div className="technologies">
-              <section className="technologies__text">
-                <p className="text">React JS, MongoDB</p>
-              </section>
-
-              <section className="technologies__iconSection">
-                <a href="#">
-                  <i className="technologies__iconSection--icon fa-solid fa-globe"></i>
-                </a>
-                <a href="#">
-                  <i className="technologies__iconSection--icon fa-brands fa-github"></i>
-                </a>
-              </section>
+            <div className="techs">
+              <span className="techs__text techsText">
+                React JS - HTML- CSS
+              </span>
+              <span className="techs__icons">
+                <i className="technologies__iconSection--icon fa-solid fa-globe icon"></i>
+                <i className="technologies__iconSection--icon fa-brands fa-github icon"></i>
+              </span>
             </div>
-          </section>
-
-          <section className="info-autor">
-            <img className="imageGirl" src={girlUser} alt="userPhoto" />
-            <p className="job">Full Stack Developer</p>
-            <p className="name">Emmelie Björklund</p>
-          </section>
-        </section>
+          </div>
+        </div>
       </section>
     </>
   );
